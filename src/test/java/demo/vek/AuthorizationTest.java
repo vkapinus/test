d16dfc8f958d17cc8de0e6be1879f494b7427745
Viktoria Kapinus <solvd@solvds-Mac-mini.local>
2021-03-09 17:55:20 +0300

    added crypto key

--- src/test/java/demo/vek/AuthorizationTest.java
+++ src/test/java/demo/vek/AuthorizationTest.java
@@ -1,6 +1,7 @@
 package demo.vek;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
+import com.qaprosoft.carina.core.foundation.utils.R;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
 import demo.gui.pages.common.HomeBasePage;
 import org.testng.Assert;
@@ -8,17 +9,14 @@ import org.testng.annotations.Test;
 
 public class AuthorizationTest extends AbstractTest {
 
-    String email = "parvikus@gmail.com";
-    String pass = "Vikadima1307";
-
     @Test
     @MethodOwner(owner = "kapinus")
     public void testAuthorization(){
         HomeBasePage homePage = initPage(getDriver(), HomeBasePage.class);
         homePage.open();
         Assert.assertTrue(homePage.isPageOpened(), "21vek home page was not opened!");
-        homePage.authorization(email, pass);
-        Assert.assertEquals(homePage.getUserEmail(), email,
+        homePage.authorization(R.TESTDATA.get("email"), R.TESTDATA.get("pass"));
+        Assert.assertEquals(homePage.getUserEmail(), R.TESTDATA.get("email"),
                 "Authorization was not successful");
     }
 }
